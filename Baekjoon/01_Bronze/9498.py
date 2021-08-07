89417092e4149db10db6055f20ebbaf78b5a71ac
JUNYOUNG <roywnsdud31@gmail.com>
2021-08-07 21:44:50 +0900

    study1 add;

---
+++ Baekjoon/01_Bronze/9498.py
@@ -0,0 +1,12 @@
+# 점수에 따른 학점 분배
+a = int(input())
+if 90 <= a :
+    print('A')
+elif 80 <= a :
+    print('B')
+elif 70 <= a :
+    print('C')
+elif 60 <= a :
+    print('D')
+else:
+    print('F')
