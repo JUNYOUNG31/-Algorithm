5abd5bd4bc2057a5e834d9fb4c0a2efc52ef1e08
JUNYOUNG <roywnsdud31@gmail.com>
2021-12-27 20:46:09 +0900

    로봇 청소기 추가

--- Baekjoon/03_Gold/14503-로봇_청소기.py
+++ Baekjoon/03_Gold/14503-로봇_청소기.py
@@ -12,3 +12,69 @@
 # 네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
 # 네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
 # 로봇 청소기는 이미 청소되어있는 칸을 또 청소하지 않으며, 벽을 통과할 수 없다.
+
+
+dr = [-1, 0, 1, 0]  # 북 서 남 동
+dc = [0, -1, 0, 1]
+
+N, M = map(int, input().split())                # 맵의 크기
+land = [[] * M for _ in range(N)]               # 맵 입력할 리스트
+r, c, d = map(int, input().split())             # 현재위치와 방향
+
+for i in range(N):
+    land[i] = list(map(int, input().split()))   # 맵 입력
+
+# for i in range(N):
+#     print(land[i])
+
+visited = [[0] * M for _ in range(N)]           # 방문 기록
+# land[r][c] = 1    
+visited[r][c] = 1                               # 방문처리하고
+cnt = 1                                         # 청소한 개수
+
+if d == 1:
+    d = 3
+elif d == 3:
+    d = 1
+
+
+def left():                                     # 왼쪽으로 돌기
+    global d                    
+    d += 1                                      # 방향 바꾸기
+    if d == 4:                                 # 인덱스 처리
+        d = 0
+
+
+turn = 0                                        # 방향
+while True:                                     
+    left()                                      # 방향전환
+    nr = r + dr[d]                              # 전진
+    nc = c + dc[d]                              # 전진
+    if visited[nr][nc] == 0 and land[nr][nc] == 0:  # 방문안하고 빈곳이면
+        visited[nr][nc] = 1                         # 방문 처리
+        # land[nr][nc] = 1
+        r = nr                                      # 이동
+        c = nc                                      # 이동
+        cnt += 1                                    # 청소 추가
+        turn = 0                                    # 방향 그대로
+        # for i in range(N):
+        #     print(visited[i])
+        # print("-------------visit-----------------")
+        # for i in range(N):
+        #     print(land[i])
+        # print("-------------land-----------------")
+        continue
+    else:
+        turn += 1                                   # 빈곳이 아니면 방향전환
+
+    if turn == 4:                                   # 4방향 다돌아도 갈곳없으면
+        nr = r + dr[(d+2) % 4]                              # 뒤로 가기
+        nc = c + dc[(d+2) % 4]                              # 뒤로 가기
+        if land[nr][nc] == 0:                       # 뒤에가 청소안한곳이면
+            r = nr                                  # 이동하고
+            c = nc
+        else:                                       # 아니면
+            break                                   # 멈춰
+        turn = 0
+
+print(cnt)
