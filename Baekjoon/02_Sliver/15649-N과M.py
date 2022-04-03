01fe8e5ca812fe3cc02ddbf58dd03e8ce2b9bff9
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-04-03 10:08:44 +0900

    N과M 완료

---
+++ Baekjoon/02_Sliver/15649-N과M.py
@@ -0,0 +1,10 @@
+# N 과 M
+
+# 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+from itertools import permutations
+
+N, M = map(int, input().split())
+P = permutations(range(1, N+1), M)
+for i in P:
+    print(' '.join(map(str, i)))
