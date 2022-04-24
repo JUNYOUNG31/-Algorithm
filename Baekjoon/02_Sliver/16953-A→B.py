421366b6bd5842fc06b361a4f62dfb78641b20e7
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-04-24 23:47:45 +0900

    A→B 완료

---
+++ Baekjoon/02_Sliver/16953-A→B.py
@@ -0,0 +1,17 @@
+# A → B
+
+
+A, B = map(int, input().split())
+cnt = 1
+while (B != A):
+    temp = B
+    if B % 10 == 1:
+        B //= 10
+    elif B % 2 == 0:
+        B //= 2
+    if temp == B:
+        print(-1)
+        break
+    cnt += 1
+else:
+    print(cnt)
