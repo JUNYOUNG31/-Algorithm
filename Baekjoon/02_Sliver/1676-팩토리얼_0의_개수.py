ce36a3800320b9f3678110da06cf7b5b54ee1c47
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-05-31 23:13:14 +0900

    팩토리얼 0의 개수 완료

---
+++ Baekjoon/02_Sliver/1676-팩토리얼_0의_개수.py
@@ -0,0 +1,13 @@
+# 팩토리얼 0의 개수
+
+N = int(input())
+
+ans = 0
+for i in range(2, N + 1):
+    if i % 5 == 0:
+        ans += 1
+    if i % 25 == 0:
+        ans += 1
+    if i % 125 == 0:
+        ans += 1
+print(ans)
