76539040bc4acc9a1eba1923a37f8e0c459cdecc
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-06-03 23:56:57 +0900

    하얀 칸 완료

---
+++ Baekjoon/01_Bronze/1100-하얀_칸.py
@@ -0,0 +1,13 @@
+# 하얀 칸
+
+chess = []
+for _ in range(8):
+    chess.append(list(map(str, list(input()))))
+
+answer = 0
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0:
+            if chess[i][j] == 'F':
+                answer += 1
+print(answer)
