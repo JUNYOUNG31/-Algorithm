941474c30cba747303f6efeaa69e5259a4899717
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-07-16 23:06:49 +0900

    가장 큰 금민수 완료

---
+++ Baekjoon/01_Bronze/1526-가장_큰_금민수.py
@@ -0,0 +1,13 @@
+# 가장 큰 금민수
+
+N = int(input())
+
+while True:
+    flag = True
+    for i in str(N):
+        if i != '4' and i != '7':
+            flag = False
+            N -= 1
+    if flag:
+        print(N)
+        break
