8f5b6b0b2b62829e9c76bc32958a29f18f528dab
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-08-26 01:40:41 +0900

    떡 먹는 호랑이 완료

--- Baekjoon/02_Sliver/2052-떡_먹는_호랑이.py
+++ Baekjoon/02_Sliver/2052-떡_먹는_호랑이.py
@@ -3,24 +3,25 @@
 D, K = map(int, input().split())
 
 dp = [0 for _ in range(D)]
-dp[0] = 1
-dp[1] = 1
+dp[0] = [1, 0]   # 1A + 0B
+dp[1] = [0, 1]   # 0A + 1B
 
+for i in range(2, D):
+    dp[i] = [dp[i-1][0] + dp[i-2][0], dp[i-1][1] + dp[i-2][1]]
+
+x, y = dp[-1][0], dp[-1][1]
+
+A, B = 1, 1
 
 while True:
-
-    for i in range(2, D):
-        dp[i] = dp[i - 1] + dp[i - 2]
-
-    if dp[D-1] == K:
+    if x*A + y*B == K:
+        print(A)
+        print(B)
         break
+    elif x*A + y*B < K:
+        B += 1
+    elif x*A + y*B > K:
+        A += 1
+        B = A
 
-    elif dp[-1] > K:
-        dp[0] += 1
-        dp[1] = dp[0]
-    else:
-        dp[1] += 1
-
-
-
-
+# print(x, y)
