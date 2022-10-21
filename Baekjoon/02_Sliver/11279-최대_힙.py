bc0f15b72ef94e3bcb4dc37373ca530a9cd692c8
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-10-22 03:45:55 +0900

    최대 힙

---
+++ Baekjoon/02_Sliver/11279-최대_힙.py
@@ -0,0 +1,17 @@
+# 최대 힙
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+heap = []
+
+for _ in range(int(input())):
+    N = int(input())
+    if N == 0:
+        if len(heap):
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (-N, N))
