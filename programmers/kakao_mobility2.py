1bf134f16fbfbe77563d4b932f9b86b9f532fdc7
JUNYOUNG31 <roywnsdud31@gmail.com>
2022-12-01 23:05:17 +0900

    수정

--- programmers/kakao_mobility2.py
+++ programmers/kakao_mobility2.py
@@ -5,24 +5,26 @@ def solution(id_list, k):
     arr = defaultdict(int)
     for i in id_list:
         temp = set(i.split())
-        print(temp)
+
         for j in temp:
             if arr[j] < k:
                 arr[j] += 1
 
     answer = 0
-    print(arr)
+    for i in arr:
+        answer += arr[i]
+
     return answer
 
 
 
 
-id_list = ["A B C D", "A D", "A B D", "B D"]
+#id_list = ["A B C D", "A D", "A B D", "B D"]
 
-k = 2
+#k = 2
 
-# ["JAY", "JAY ELLE JAY MAY", "MAY ELLE MAY", "ELLE MAY", "ELLE ELLE ELLE", "MAY"]
-# k =3
+id_list = ["JAY", "JAY ELLE JAY MAY", "MAY ELLE MAY", "ELLE MAY", "ELLE ELLE ELLE", "MAY"]
+k =3
 
 print(solution(id_list, k))
 
