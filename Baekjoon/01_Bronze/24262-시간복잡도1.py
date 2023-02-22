65d4d6abf24a437c23921e1ebf41d000f243fe32
JUNYOUNG31 <roywnsdud31@gmail.com>
2023-02-22 23:32:20 +0900

    b 2

---
+++ Baekjoon/01_Bronze/24262-시간복잡도1.py
@@ -0,0 +1,11 @@
+# 시간복잡도 1 문제
+
+
+def MenOfPassion(n):
+    A = []
+    i = [n / 2]
+    return A[i]; # 코드1
+
+
+print(1)
+print(0)
---
+++ Baekjoon/01_Bronze/24263-시간복잡도2.py
@@ -0,0 +1,11 @@
+# 시간복잡도 2
+A = []
+def MenOfPassion(A, n):
+    hap = 0
+    for i in range(n):
+        hap = hap + A[i]
+    return hap
+
+
+print(input())
+print(1)
