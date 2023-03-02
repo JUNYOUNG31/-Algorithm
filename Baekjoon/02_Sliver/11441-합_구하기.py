dda1ef3875187593587c2ba42ec7b81782e80c80
JUNYOUNG31 <roywnsdud31@gmail.com>
2023-03-02 23:58:23 +0900

    합 구하기

---
+++ Baekjoon/02_Sliver/11441-합_구하기.py
@@ -0,0 +1,20 @@
+# 합 구하기
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+arr = list(map(int, input().split()))
+ans = [0] * (N+1)
+
+for k in range(N):
+    ans[k] = ans[k-1]+arr[k]
+
+M = int(input())
+
+for k in range(M):
+    i, j = map(int, input().split())
+
+    res = ans[j-1]-ans[i-2]
+    sys.stdout.write(str(res)+'\n')
