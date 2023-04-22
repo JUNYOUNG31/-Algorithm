cbc9470a2518501488a776f9d045cf259f7d8459
JUNYOUNG31 <roywnsdud31@gmail.com>
2023-04-22 23:02:07 +0800

    플로이드 수정

--- Baekjoon/03_Gold/11404-플로이드.py
+++ Baekjoon/03_Gold/11404-플로이드.py
@@ -1,9 +1,11 @@
 # 플로이드
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
-# 노드의 개수 및 간선의 개수를 입력받기
 n = int(input())
 m = int(input())
+
 # 2차원 리스트를 만들고, 무한으로 초기화
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 for a in range(1, n + 1):
@@ -26,15 +28,10 @@ for k in range(1, n +1):
                 graph[i][j] = graph[i][k] + graph[k][j]
 
 
-# 수행된 결과를 출력
 for a in range(1, n + 1):
     for b in range(1, n + 1):
-        # 도달할 수 없는 경우, 무한이라고 출력
         if graph[a][b] == INF:
             print(0, end=" ")
-        # 도달할 수 있는 경우 거리를 출력
         else:
             print(graph[a][b], end=" ")
     print()
-
-
